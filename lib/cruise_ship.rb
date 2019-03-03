0f9cb73a3c3bd62b4bcb6ac8c32e0a9a7f6f1c09
Joy Lin <vvlnote@hotmail.com>
2019-03-03 17:15:10 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,4 +8,11 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  name_str = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      name_str = name
+    end
+  end
+  name_str
 end
