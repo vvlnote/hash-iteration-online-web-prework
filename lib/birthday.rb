0930e41d629c1c255af002755ee42d325fbac2cb
Joy Lin <vvlnote@hotmail.com>
2019-03-03 17:09:26 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,9 +7,8 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |name, age|
-    puts "" 
+    puts "Happy Birthday #{name}! You are now #{age} years old!" 
   end
-  Happy Birthday Timmy! You are now 9 years old!
 end
 
 
